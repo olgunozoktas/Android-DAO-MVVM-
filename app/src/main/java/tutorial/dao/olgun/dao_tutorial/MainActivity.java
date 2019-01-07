b8808a033194bdfc77156a8a093c2d8f715125ba
OLGUNOZOKTAS <olgun_ozoktas@hotmail.com>
2019-01-08 00:12:05 +0200

    NotViewModel (ViewModel) class has been created, Observer Created in the MainActivity class

--- app/src/main/java/tutorial/dao/olgun/dao_tutorial/MainActivity.java
+++ app/src/main/java/tutorial/dao/olgun/dao_tutorial/MainActivity.java
@@ -1,13 +1,46 @@
 package tutorial.dao.olgun.dao_tutorial;
 
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModelProviders;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private NoteViewModel noteViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //We don't create new instance of the NoteViewModel because, this will cause to create
+        //new instance in every activity and we do not want that,
+        //Instead we have to ask Android System for ViewModel
+        //Because System knows when to create a new model instance or when to provide existence instance
+        noteViewModel = ViewModelProviders.of(this).get(NoteViewModel.class);
+
+        //ViewModelProviders.of(this) - ViewModel will know which lifecycle it has to be follow
+        //When this activity is destroyed the ViewModel instance will be destroyed automatically
+        noteViewModel.getAllNotes().observe(this, new Observer<List<Note>>() {
+
+            //Whenever a change is been made on the list, then this method will be called
+            //observe method is specific to the LiveData<>
+
+            //Even whenever the orientation of the device is changed, because of that everything will be cleared
+            //This method will be called again
+            //But because of that we are not creating a new instance of the NoteViewModel
+            //The data still available in the there, and we are getting the same data
+            //Instead of requesting database again, this is the benefit of the using ViewModel
+            @Override
+            public void onChanged(@Nullable List<Note> notes) {
+                //update RecyclerView
+                Toast.makeText(MainActivity.this, "onChanged", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
